6da28e85c6e3c7e7ada00e311a280bd8f91d965c
Yevgeniy <yevgeniy.shin.v@gmail.com>
2022-10-19 22:57:50 +0300

    Add email and password to login screen

---
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by boockich mac on 19.10.2022.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+
+}
